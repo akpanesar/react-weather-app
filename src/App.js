b5ef72ff1437dd742815462eaa1361ff6d4b38ab
AKPanesar <88969540+akpanesar@users.noreply.github.com>
2021-12-11 21:05:38 +0000

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Weather from "./Weather";
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <h1> Weather App</h1>
         <Weather />
+        <Footer />
       </header>
     </div>
   );
---
+++ src/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <p>
+      Coded by
+      <a
+        href="https://www.instagram.com/amanvalpanesar/"
+        target="_blank"
+        title="instagram Profile"
+      >
+        Amanpreet Panesar
+      </a>
+      open-sourced on
+      <a
+        href="https://github.com/akpanesar/Portfolio"
+        target="_blank"
+        title="Github Profile"
+      >
+        Github
+      </a>
+      hosted on
+      <a href="https://www.netlify.com/" target="_blank" title="Netlify">
+        Netlify
+      </a>
+    </p>
+  );
+}
