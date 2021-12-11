3364076b0c1647b67c1f856e7926c8dcae428549
AKPanesar <88969540+akpanesar@users.noreply.github.com>
2021-12-11 20:57:53 +0000

    axios installed and search bar working

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-export default function Weather() {
-  return <p>Hello from Weather App </p>;
+export default function WeatherSearch() {
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState({});
+
+  function showWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function showTemperature(event) {
+    event.preventDefault();
+    let apiKey = "3bf3d898af236340eac60ab5658c130c";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(showWeather);
+  }
+  function handleCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={showTemperature}>
+      <input
+        type="search"
+        placeholder=" Enter a city..."
+        onChange={handleCity}
+      />
+      <input type="submit" value="Search" />
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <h4>{city}</h4>
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
